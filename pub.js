a4a9920704b3499151be8e5df08b6f378a565804
nayem9b <razwanniam1@gmail.com>
2023-09-18 01:27:59 +0600

    subsription added

--- pub.js
+++ pub.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const redis = require("redis");
+
 const app = express();
 
 let publisher = redis.createClient({
   url: "redis://localhost:6379",
 });
 
-publisher.on("error", (err) => console.log("Redis error"));
-publisher.on("connect", (conn) => console.log("Redis connected"));
+publisher.on("error", (err) => console.log("Redis Error"));
+publisher.on("connect", (err) => console.log("Redis Connected"));
 
 const connect = async () => {
   await publisher.connect();
 };
 
+connect();
+
 app.get("/", (req, res) => {
   res.send({
-    message: "publisher active on port 3001",
+    message: "Publisher active from port 3001",
   });
 });
 
@@ -23,17 +26,16 @@ app.get("/publish", async (req, res) => {
   const id = Math.floor(Math.random() * 10);
   const data = {
     id,
-    message: `message ${id}`,
+    message: `message - ${id}`,
   };
-
+  console.log("publish data: ", data);
   await publisher.publish("message", JSON.stringify(data));
   res.send({
-    message: "data published",
+    message: "data published!",
+    data,
   });
 });
 
-connect();
-
 app.listen(3001, () => {
-  console.log("Publisher server started on 3001");
+  console.log("Publisher server start on 3001");
 });
